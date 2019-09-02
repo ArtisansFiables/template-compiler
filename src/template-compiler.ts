f9a04dfb9cab43818e40cc6a2c62afdb34d19026
Devessier <baptiste@devessier.fr>
2019-09-02 13:07:41 +0200

    Finish library and add tests

--- src/template-compiler.ts
+++ src/template-compiler.ts
@@ -1,4 +1,38 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-// import "core-js/fn/array.find"
-// ...
-export default class DummyClass {}
+import { promises as fs } from 'fs'
+import { join } from 'path'
+import { compileFile, compileTemplate } from 'pug'
+
+const toPascalCase = require('pascal-case')
+
+class Walker {
+    files: Map<string, compileTemplate> = new Map()
+
+    async walk(rootDir: string, rootDirName: string) {
+        const entries = await fs.readdir(rootDir, { withFileTypes: true })
+
+        for (const entry of entries) {
+            const path = join(rootDir, entry.name)
+            const filename = `${rootDirName} ${entry.name}`
+
+            if (entry.isDirectory()) {
+                await this.walk(path, filename)
+            } else if (entry.isFile() && !entry.name.startsWith('_')) {
+                const key = toPascalCase(filename.split('.')[0])
+                const template = compileFile(path)
+
+                this.files.set(key, template)
+            }
+        }
+    }
+}
+
+export async function compile(
+    root: string,
+    baseName?: string
+): Promise<Map<string, compileTemplate>> {
+    const walker = new Walker()
+
+    await walker.walk(root, baseName || '')
+
+    return walker.files
+}
--- test/template-compiler.test.ts
+++ test/template-compiler.test.ts
@@ -1,14 +1,51 @@
-import DummyClass from '../src/template-compiler'
+import { compileTemplate } from 'pug'
 
-/**
- * Dummy test
- */
-describe('Dummy test', () => {
-  it('works if true is truthy', () => {
-    expect(true).toBeTruthy()
-  })
+import { compile } from '../src/template-compiler'
 
-  it('DummyClass is instantiable', () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
-  })
+import { join } from 'path'
+
+describe('Library runs correctly', () => {
+    it('returns correct number of templates', async () => {
+        const templates = await compile(join(__dirname, './templates_1'))
+
+        expect(templates.size).toBe(3)
+    })
+
+    it('returns templates we expect', async () => {
+        const templates = await compile(join(__dirname, './templates_1'))
+        const keys: string[] = [...templates.keys()]
+
+        expect(keys).toEqual(['Body', 'HeadersDirLololol', 'HeadersHeader'])
+    })
+
+    it('adds prefix to all files', async () => {
+        const PREFIX = 'prefix'
+
+        const templates = await compile(join(__dirname, './templates_1'), PREFIX)
+        const keys: string[] = [...templates.keys()]
+
+        expect(keys).toEqual(['PrefixBody', 'PrefixHeadersDirLololol', 'PrefixHeadersHeader'])
+    })
+
+    it('returns compileTemplate instances', async () => {
+        const templates = await compile(join(__dirname, './templates_1'))
+
+        const render = templates.get('Body')
+        if (render === undefined) return
+
+        expect(render()).toBe('')
+    })
+})
+
+describe('Pug can be used correctly', () => {
+    it('template inheritance works', async () => {
+        const templates = await compile(join(__dirname, './templates_2'))
+
+        const render = templates.get('Index')
+        if (render === undefined) return
+
+        expect(render()).toBe(
+            '<html><body><p>This is index</p><p>This is a layout</p></body></html>'
+        )
+    })
 })
